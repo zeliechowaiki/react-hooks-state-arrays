1d35c47ebf89f4b4c096a40d23de5bf091ad9342
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-23 17:07:55 -0500

    commit tree fed6c51a9fc6e09046a0c091a4f7c41e230135f1

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,14 +3,51 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  // const [filterBy, setFilterBy] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
+  function handleFilterChange(selectedCuisine) {
+    if (selectedCuisine === 'All') {
+      setFoods(foods);
+    }
+    else {
+      setFoods(foods.filter(food => {
+        return food.cuisine === selectedCuisine;
+      }))
+    }
+  }
+
+  // function handleLiClick(id) {
+  //   const newFoodArray = foods.filter(food => food.id !== id);
+  //   setFoods(newFoodArray);
+  // }
+
+  function handleHeatClick(id) {
+    const newFoodArray = foods.map(food => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1
+        };
+      }
+      else {
+        return food;
+      }
+    })
+    setFoods(newFoodArray);
+  }
+
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} 
+    // onClick={() => handleLiClick(food.id)}
+    onClick={() => handleHeatClick(food.id)}
+    >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -18,6 +55,13 @@ function SpicyFoodList() {
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+       <select name="filter" onChange={(e) => handleFilterChange(e.target.value)}>
+         <option value="All">All</option>
+         <option value="American">American</option>
+         <option value="Sichuan">Sichuan</option>
+         <option value="Thai">Thai</option>
+         <option value="Mexican">Mexican</option>
+       </select>
       <ul>{foodList}</ul>
     </div>
   );
